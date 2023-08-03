45ead27d8f2d8d5a59138b481e95018a5e667007
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-08-03 08:57:20 +0530

    update app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,113 +1,132 @@
 <script>
 	import { onMount } from 'svelte';
-	import { Input, Label, Checkbox, Button } from 'flowbite-svelte';
+	import { Input, Label, Button } from 'flowbite-svelte';
 	import 'flowbite/dist/flowbite.css';
   
-	let companyName = '';
-	let companyLogo = '';
-	let address = '';
+	let id = '';
+	let name = '';
+	let address = {
+	  addressLine: '',
+	  cityName: '',
+	  regionName: '',
+	  postCode: '',
+	  countryCode: '',
+	  latitude: '',
+	  longitude: ''
+	};
 	let phone = '';
 	let website = '';
-	let dateFormat = '';
-	let timezone = '';
+	let currencyCode = '';
+	let preferredDateFormat = '';
+	let timeZone = '';
+	let preferredCountryCode = '';
+	let preferredCountries = [];
+	let dataFetched = false;
 	let currencies = [];
-    let currencyCode = '';
-	let timezones = [];
-	let logoImage = new Image();
+	let timeZones = [];
+  
+	async function fetchData() {
+	  try {
+		const response = await fetch("https://api.recruitly.io/api/business/profile?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77");
+		const data = await response.json();
+		console.log(data);
+  
+		id = data.id;
+		name = data.name;
+		address = {
+		  addressLine: data.address.addressLine,
+		  cityName: data.address.cityName,
+		  regionName: data.address.regionName,
+		  postCode: data.address.postCode,
+		  countryCode: data.address.countryCode,
+		  latitude: data.address.latitude,
+		  longitude: data.address.longitude
+		};
+		phone = data.phone;
+		website = data.website;
+		currencyCode = data.currencyCode.code;
+		preferredDateFormat = data.preferredDateFormat;
+		timeZone = data.timeZone;
+		preferredCountryCode = data.preferredCountryCode;
+		preferredCountries = [data.preferredCountries];
+		dataFetched = true;
+	  } catch (error) {
+		console.error("Error fetching data:", error);
+	  }
+	}
 
-	
- // Function to fetch default values from API
- const fetchDefaultValues = async () => {
-    try {
-      const response = await fetch("https://api.recruitly.io/api/business/profile?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77");
-      const data = await response.json();
-      console.log('Default Values API response:', data);
-
-      // Populate the input fields with fetched data
-      companyName = data.name;
-      address = data.fullAddressLine;
-      phone = data.phone;
-      website = data.website;
-      currencyCode = data.currencyCode.code;
-      dateFormat = data.preferredDateFormat;
-      timezone = data.timeZone;
-    } catch (error) {
-      console.error("Failed to fetch default values:", error);
-    }
-  };
-
-	onMount(async () => {
-		await fetchDefaultValues();
+	async function fetchCurrencies() {
     try {
       const response = await fetch("https://api.recruitly.io/api/lookup/currency?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA");
-      const data = await response.json();
-	  console.log('API response:', data);
-      currencies = data.data;
-	  console.log('Currencies type:', typeof currencies);
+      const currencyData = await response.json();
+      currencies = currencyData.data;
     } catch (error) {
-      console.error("Failed to fetch currencies:", error);
+      console.error("Error fetching currencies:", error);
     }
-
-	    // Fetch timezones from the API
-		const response = await fetch('https://api.recruitly.io/api/lookup/timezone?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA');
-    if (response.ok) {
-      const data = await response.json();
-      timezones = data.data; // Assuming the API returns an array of timezones in the format: [{ code: '...', name: '...' }, ...]
-    } else {
-      console.error('Failed to fetch timezones from the API');
-    }
-	logoImage.src = 'https://s3-eu-west-1.amazonaws.com/recruitly-public/334fbbc8-6ab1-49d1-bede-5ead2a0b1a56/43aa5940-e9b2-406a-a826-99a405a7a343.jpeg';
-  });
-  
-  const handleSubmit = async () => {
-  try {
-    const response = await fetch("https://api.recruitly.io/api/business/profile/save?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        systemRecordLabel: companyName,
-        fullAddressLine: address,
-        phone,
-        website,
-        currencyCode,
-        preferredDateFormat: dateFormat,
-        timeZone: timezone,
-        // Add other properties as needed
-      }),
-    });
-
-	const responseData = await response.json(); // Try to get response data even if the request is not successful
-  console.log("Response Status:", response.status);
-  console.log("Response Data:", responseData);
-
-    if (response.ok) {
-      console.log("Profile updated successfully");
-    } else {
-      console.error("Failed to update profile");
-    }
-  } catch (error) {
-    console.error("Error updating profile:", error);
   }
-};
 
+  async function fetchTimeZones() {
+    try {
+      const response = await fetch("https://api.recruitly.io/api/lookup/timezone?apiKey=TEST27306FA00E70A0F94569923CD689CA9BE6CA");
+      const timeZoneData = await response.json();
+	  console.log(timeZoneData);
+      timeZones = timeZoneData.data;
+    } catch (error) {
+      console.error("Error fetching time zones:", error);
+    }
+  }
+  
+	onMount(() => {
+	  fetchData();
+	  fetchCurrencies();
+	  fetchTimeZones();
+	});
+  
+	async function saveFormData() {
+	  const updateData = {
+		id,
+		name,
+		address: {
+		  addressLine: address.addressLine,
+		  cityName: address.cityName,
+		  regionName: address.regionName,
+		  postCode: address.postCode,
+		  countryCode: address.countryCode,
+		  latitude: address.latitude,
+		  longitude: address.longitude
+		},
+		phone,
+		website,
+		currencyCode: {
+		  code: currencyCode
+		},
+		preferredDateFormat,
+		timeZone,
+		preferredCountries,
+		preferredCountryCode
+	  };
+  
+	  try {
+		const response = await fetch('https://api.recruitly.io/api/business/profile/save?apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77', {
+		  method: "POST",
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		  body: JSON.stringify(updateData),
+		});
+  
+		const updatedData = await response.json();
+		console.log("Updated data from API:", updatedData);
+		alert("Business Profile Updated successfully");
+	  } catch (error) {
+		console.error("Error updating data:", error);
+	  }
+	}
+  
   </script>
   
   <style>
-  
-	.form-container {
-	  width: 400px; /* Adjust the width as needed */
-	  background-color: white;
-	  border-radius: 8px;
-	  padding: 20px;
-	  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-	  justify-self: center;
-      align-self: center;
-	  margin-left: 500px;
-	}
-
-	h1 {
+		h1 {
     text-align: center;
     font-size: 1.5em;
     font-weight: bold;
@@ -117,65 +136,97 @@
   
   <div class="form-container">
 	<h1 style="text-align: center;">Business Profile</h1>
-  <form on:submit|preventDefault={handleSubmit}>
-	<div class="grid gap-6 mb-6 md:grid-cols-1">
-		<div>
-			<Label for="company_logo" class="mb-2">Company logo</Label>
-			<img bind:this={logoImage} alt="Company Logo" style="max-width: 100px; max-height: 100px; margin-top: 10px;" />
-		</div>
+	{#if dataFetched}
+	  <form on:submit|preventDefault={saveFormData}>
+  <div>
+    <Label for="company_name" class="mb-2">Company name</Label>
+    <Input type="text" id="company_name" bind:value={name} required />
+  </div>
 
-	    <div>
-		    <Label for="company_name" class="mb-2">Company name</Label>
-		    <Input type="text" id="company_name" bind:value={companyName} required />
-	    </div>
+  <div>
+    <Label for="addressLine" class="mb-2">Address Line</Label>
+    <Input type="text" id="addressLine" bind:value={address.addressLine} required />
+  </div>
 
-	    <div>
-		<Label for="address" class="mb-2">Address</Label>
-		<Input type="text" id="address" bind:value={address} required />
-	  </div>
+  <div>
+    <Label for="cityName" class="mb-2">City Name</Label>
+    <Input type="text" id="cityName" bind:value={address.cityName} required />
+  </div>
 
-	  <div>
-		<Label for="phone" class="mb-2">Phone number</Label>
-		<Input type="tel" id="phone" bind:value={phone} placeholder="12345-67890"  title="Please use the format XXXXX-XXXXX or XXXXXXXXXX" required />
-	  </div>
+  <div>
+    <Label for="regionName" class="mb-2">Region Name</Label>
+    <Input type="text" id="regionName" bind:value={address.regionName} required />
+  </div>
 
-	  <div>
-		<Label for="website" class="mb-2">Website URL</Label>
-		<Input type="url" id="website" bind:value={website} placeholder="flowbite.com" required />
-	  </div>
+  <div>
+    <Label for="postCode" class="mb-2">Post Code</Label>
+    <Input type="text" id="postCode" bind:value={address.postCode} required />
+  </div>
 
-	  <div>
-		<Label for="currency" class="mb-2">Currency</Label>
-		<select id="currency" bind:value={currencyCode} required>
-			<option value="">Select Currency</option>
-		  {#each currencies as currency}
-		  <option value={currency.code}>{currency.name}</option>
-		  {/each}
-		</select>
-	  </div>
-	  
-	  <div>
-		<Label for="date_format" class="mb-2">Preferred date format</Label>
-		<select id="date_format" bind:value={dateFormat} required>
-			<option value="">Select Date Format</option>
-			<option value="default">Default</option>
-			<option value="pretty">Pretty</option>
-			<option value="date">Date Only</option>
-			<option value="datetime">Date and Time Only</option>
-		</select>
-	</div>
-	  
-	  <div>
-		<Label for="timezone" class="mb-2">Timezone</Label>
-		<select id="timezone" bind:value={timezone} required>
-		  <option value="">Select Timezone</option>
-		  {#each timezones as timezone}
-			<option value={timezone.code}>{timezone.name}</option>
-		  {/each}
-		</select>
-	  </div>	
-	    
-	</div>
-	<Button type="submit" style="background-color: #007bff;">Update Profile</Button>
-  </form>
-</div>
+  <div>
+    <Label for="countryCode" class="mb-2">Country Code</Label>
+    <Input type="text" id="countryCode" bind:value={address.countryCode} required />
+  </div>
+
+  <div>
+    <Label for="latitude" class="mb-2">Latitude</Label>
+    <Input type="text" id="latitude" bind:value={address.latitude} required />
+  </div>
+
+  <div>
+    <Label for="longitude" class="mb-2">Longitude</Label>
+    <Input type="text" id="longitude" bind:value={address.longitude} required />
+  </div>
+
+  <div>
+    <Label for="phone" class="mb-2">Phone number</Label>
+    <Input type="tel" id="phone" bind:value={phone} placeholder="12345-67890" title="Please use the format XXXXX-XXXXX or XXXXXXXXXX" required />
+  </div>
+
+  <div>
+    <Label for="website" class="mb-2">Website URL</Label>
+    <Input type="url" id="website" bind:value={website} placeholder="flowbite.com" required />
+  </div>
+
+  <div>
+    <Label for="currencyCode" class="mb-2">Currency Code</Label>
+    <select id="currencyCode" bind:value={currencyCode} required>
+      {#each currencies as currency}
+        <option value={currency.code}>{currency.name} ({currency.code})</option>
+      {/each}
+    </select>
+  </div>
+
+  <div>
+    <Label for="preferredDateFormat" class="mb-2">Preferred Date Format</Label>
+    <Input type="text" id="preferredDateFormat" bind:value={preferredDateFormat} required />
+  </div>
+
+  <div>
+	<Label for="timeZone" class="mb-2">Time Zone</Label>
+	<select id="timeZone" bind:value={timeZone} required>
+	  {#each timeZones as timeZoneOption}
+		<option value={timeZoneOption.code}>{timeZoneOption.name}</option>
+	  {/each}
+	</select>
+  </div>
+  
+
+  <div>
+    <Label for="preferredCountryCode" class="mb-2">Preferred Country Code</Label>
+    <Input type="text" id="preferredCountryCode" bind:value={preferredCountryCode} required />
+  </div>
+
+  <div>
+    <Label for="preferredCountries" class="mb-2">Preferred Countries</Label>
+    <Input type="text" id="preferredCountries" bind:value={preferredCountries} required />
+  </div>
+
+  <Button type="submit" style="background-color: #007bff;">Update Profile</Button>
+</form>
+
+	{:else}
+	  <p>Loading data...</p>
+	{/if}
+  </div>
+  
